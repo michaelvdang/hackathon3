40791dd9d0aa72ae19bfdeaca16b71a33a3e152d
Michael Dang <78937821+michaelvdang@users.noreply.github.com>
2024-08-24 22:37:42 -0700

    Split convo into divs, changing commentsMap key to array<num,num>

--- components/Transcript.tsx
+++ components/Transcript.tsx
@@ -32,41 +32,34 @@ const Transcript: React.FC<TranscriptProps> = ({
   setNewComment,
 })  => {
 
-  const [content, setContent] = useState<string>('');
-  // const [content, setContent] = useState<string>(
-  //   `0123456789\n
-  //   0123456789\n0123456789\n0123456789\n0123456789\n
-  //   0123456789\n0123456789\n0123456789\n0123456789\n
-  //   0123456789\n0123456789\n0123456789\n`
-  //   // 'Hello, my name is John and I have a friend name .'
-  // );
-
-  // const parentRef = useRef<HTMLDivElement>(null);
-  // const [parentWidth, setParentWidth] = useState<number>(0);
+  const [content, setContent] = useState<JSX.Element[]>([]);
+  // const [content, setContent] = useState<string>('');
 
   useEffect(() => {
-    setContent(conversation.messages.map(message => message.role + ": " + message.content).join("\n\n"));
+    const messages = (conversation.messages.map((message, index) => (
+      <div key={index} style={{ paddingBottom: '10px'}}>
+        <span style={{ fontWeight: 'bold' }}>
+          {message.role}
+        </span>
+        { ": "}
+        <span onClick={e => handleSpanClick(e, index)} style={{ cursor: 'pointer' }}>
+          {message.content}
+        </span>
+      </div>
+    )));
+
+    setContent(messages);
   }, []);
   
-  // useEffect(() => {
-  //   if (parentRef.current) {
-  //     // Option 1: Using offsetWidth
-  //     const width = parentRef.current.offsetWidth;
-
-  //     // Option 2: Using getBoundingClientRect
-  //     // const width = parentRef.current.getBoundingClientRect().width;
-
-  //     setParentWidth(width);
-  //   }
-  // }, []);
 
   useEffect(() => {
     console.log('commentsMap: ', commentsMap);
   }, [commentsMap]);
   
 
-  const handleDivClick = (e: React.MouseEvent<HTMLDivElement>): void => {
+  const handleSpanClick = (e: React.MouseEvent<HTMLSpanElement>, index: number): void => {
     e.preventDefault();
+    console.log('index: ', index);
 
     // Get cursor position
     const selection = window.getSelection();
@@ -122,9 +115,9 @@ const Transcript: React.FC<TranscriptProps> = ({
         {/* Displaying the actual content in the foreground */}
         <div
           className="absolute  h-full w-full left-0 right-0 top-0 border-2 border-white"
-          onClick={handleDivClick}
+          // onClick={handleDivClick}
         >
-          <span style={{ opacity: '0'}}>{content}</span>
+          {content}
         </div>
         {/* {showPopup && (
           <AddCommentPopupDialog
